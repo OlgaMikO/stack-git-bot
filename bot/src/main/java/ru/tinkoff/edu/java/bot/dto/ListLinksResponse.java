29bfa4bad7d9b077cd95a5ca8fe8b6387f9bf00b
OlgaMikO <86295022+OlgaMikO@users.noreply.github.com>
2023-05-07 21:23:21 +0300

    Deleted unused import

--- bot/src/main/java/ru/tinkoff/edu/java/bot/dto/ListLinksResponse.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/dto/ListLinksResponse.java
@@ -3,7 +3,6 @@ package ru.tinkoff.edu.java.bot.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import ru.tinkoff.edu.java.scrapper.dto.response.LinkResponse;
 
 import java.util.ArrayList;
 
