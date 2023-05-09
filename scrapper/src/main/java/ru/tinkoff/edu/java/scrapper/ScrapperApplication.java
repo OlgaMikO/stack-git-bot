615ad10bfcdc4fce6a93dcdcd97bbae0fdd90783
OlgaMikO <86295022+OlgaMikO@users.noreply.github.com>
2023-05-09 18:39:13 +0300

    Changed scrapper.yml

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -15,7 +15,6 @@ public class ScrapperApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
-        System.out.println(config);
 
         GitHubApiClient gitHubApiClient = ctx.getBean(ClientConfiguration.class).gitHubApiClient();
         System.out.println(gitHubApiClient.fetchRepository("OlgaMikO", "stack-git-bot"));
